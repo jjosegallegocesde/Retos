93bad9596945a66e6ef27ec8c68ec18c36771bf4
Juan <juan.gallegomesa@gmail.com>
2021-06-17 08:45:02 -0500

    se resuelve retos 2

--- src/retos/Retos.java
+++ src/retos/Retos.java
@@ -115,7 +115,7 @@ public class Retos {
         /* Un terreno cuadrado tiene un área de N m2 ¿Cuántos metros de alambre se necesitan para cercarlo?*/
  
         //entradas
-        Scanner entrada = new Scanner(System.in);
+        /*Scanner entrada = new Scanner(System.in);
         
         System.out.println("Digite el area: ");
         int area=entrada.nextInt();
@@ -125,14 +125,132 @@ public class Retos {
         double cercado=lado*4;
         
         //salida
-        System.out.println("la cantidad de alambre es de: "+cercado);
+        System.out.println("la cantidad de alambre es de: "+cercado);*/
+        
+        
+        
+        /*Construir un programa que reciba números enteros y los sume mientras estos sean positivos, si se 
+        digita un número negativo el programa debe terminar*/
+        
+        /*Scanner entrada = new Scanner(System.in);
+        
+        int numero=1;
+        int contador=0;
+        
+        
+        while(numero>=0){
+            
+            System.out.println("Por favor digite un numero entero: ");
+            numero=entrada.nextInt();
+            
+            if(numero>=0){
+                contador=contador+numero;
+            }
+            
+                 
+        }
+        
+        System.out.println("El contador quedo en: "+(contador));*/
         
         
         
         
+        /*Codificar un programa que presente un menú de 4 opciones y reciba un numero natural  para realizar 
+            las siguientes operaciones:
+            0: Salida
+            1: Encuentre si el número es múltiplo de 2
+            2: Encuentre la raíz cuadrada del número
+            3: Sume 100 al número ingresado
+            4: Eleve a la 2 el número ingresado */ 
+        
+           /*int opcion;
+           Scanner entrada = new Scanner(System.in);
+           
+           
+        
+           do{
+               
+               System.out.println("Bienvenido \n");
+               
+               System.out.println("0: Salida");
+               System.out.println("1: Multiplo 2");
+               System.out.println("2: raiz cuadrada");
+               System.out.println("3: suma 100");
+               System.out.println("4: potencia 2");
+               
+               System.out.print("Digite una opcion: ");
+               opcion=entrada.nextInt();
+               
+               switch(opcion){
+                   
+                   case 0: 
+                       break;
+                       
+                   case 1:
+                       
+                       System.out.print("\n Digite un numero entero: ");
+                       int numero=entrada.nextInt();
+                       
+                       int resto= numero%2; //% operador modulo
+                       
+                       if(resto==0){
+                           System.out.println("El "+numero+" es multiplo de 2");
+                       }else{
+                           System.out.println("El "+numero+" No es multiplo de 2");
+                       } 
+                       break;
+                    
+                   case 2:
+                       
+                       System.out.print("\n Digite un numero entero: ");
+                       int numero2=entrada.nextInt();
+                      
+                       if(numero2>=0){
+                           double raiz=Math.sqrt(numero2);
+                           System.out.println("La raiz cuadrada de "+numero2+" es: "+ raiz);
+                       }else{
+                           System.out.println("Digite un número positivo");
+                       }
+                       
+                    break;
+                    
+                   case 3:
+                       System.out.print("\n Digite un numero entero: ");
+                       int numero3=entrada.nextInt();
+                       
+                       System.out.println("El "+numero3+" +100="+(numero3+100));
+                       
+                    break;
+                    
+                   case 4:
+                       
+                       System.out.print("\n Digite un numero entero: ");
+                       int numero4=entrada.nextInt();
+                       
+                       System.out.println("El numero "+numero4+"elevado a la 2 es: "+(Math.pow(numero4, 2)));
+                       
+                       break;
+                     
+                   
+               }
+               
+           }while(opcion!=0);*/
         
         
         
+        /* Mostrar los números del 1 al 200 saltando de 12 en 12(1,12,24…)*/
+        
+        /*for(int i=1; i<=200; i=i+12){
+            
+            System.out.println("Tabla de 12: 1");
+            
+            System.out.println("Tabla del 12: "+i);
+            
+       
+        }*/
+        
+        
+        /*Pedir 20 números y contar cuantos de los ingresados fueron negativos*/
         
         
         
